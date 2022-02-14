6df83698b325ca69aef5ece38b8fc124cb0a2809
Guillaume Viger <fenekku@fenekku.com>
2022-02-14 16:36:59 -0500

    release: v0.2.5

--- invenio_requests/version.py
+++ invenio_requests/version.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2021 CERN.
 # Copyright (C) 2021 TU Wien.
+# Copyright (C) 2022 Northwestern University.
 #
 # Invenio-Requests is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,4 +13,4 @@ This file is imported by ``invenio_requests.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
